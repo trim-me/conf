37f89706ea72e96860553c01188c37b89676cb1b
cabbage <chinesebigcabbage@gmail.com>
2021-09-13 15:02:58 +0800

    add exec file path find

--- driver/file_stat.go
+++ driver/file_stat.go
@@ -3,18 +3,27 @@ package driver
 import (
 	"log"
 	"os"
+	"path/filepath"
 )
-
 
 //verify file isExist
 func FileStat(file string) string {
 	var (
 		str, _          = os.Getwd()
 		defaultConfPath = str + file
+		execDir         string
+		err             error
 	)
-	_, err := os.Stat(defaultConfPath)
+	_, err = os.Stat(defaultConfPath)
 	if os.IsNotExist(err) {
-		log.Fatal(err)
+		if execDir, err = filepath.Abs(filepath.Dir(os.Args[0])); err != nil {
+			log.Fatal("get exec file path failed")
+		}
+		defaultConfPath = execDir + file
+		_, err = os.Stat(defaultConfPath)
+		if os.IsNotExist(err) {
+			log.Fatal(err)
+		}
 	}
 	return defaultConfPath
 }
