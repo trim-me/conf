19d09cfe2861d08c249539b86cbfd4d26407893e
cabbage <chinesebigcabbage@gmail.com>
2022-03-28 15:46:49 +0800

    modify main function of examples conf test

--- examples/test/conf.go
+++ examples/test/conf.go
@@ -6,43 +6,29 @@ import (
 	"github.com/trim-me/conf/examples/test/types"
 )
 
-var ConfMap AppConfMap
-
-type AppConfMap struct {
-	App struct {
-		HttpPort int    `toml:"http_port"`
-		Mode     string `toml:"mode"`
-		Limit    int    `toml:"limit"`
-	} `toml:"app"`
-	Domain struct {
-		AdminDomain string   `toml:"admin_domain"`
-		WebDomain   []string `toml:"web_domain"`
-		ImageUrl    string   `toml:"image_url"`
-	} `toml:"domain"`
-	Token struct {
-		TokenName   string `toml:"token_name"`
-		TokenExpire int    `toml:"token_expire"`
-	} `toml:"Token"`
-	Path struct {
-		LogPathRoot string `toml:"log_path_root"`
-		PathRoot    string `toml:"path_root"`
-	} `toml:"Path"`
-}
+var (
+	ConfMap    types.AppConfMap
+	GlobalConf types.GlobalConfMap
+)
 
 func main() {
-	var AppConfPath = "/conf/app/conf.toml"
 	//get api conf
-	driver.InitConfToml(AppConfPath, &ConfMap)
-	fmt.Println(ConfMap)
+	AppConfToml()
 
 	//get global conf
 	GlobalConfToml()
 
 }
+func AppConfToml() {
+	AppConfPath := "/conf/app/conf.toml"
+	//get app conf
+	driver.InitConfToml(AppConfPath, &ConfMap)
+	fmt.Println(ConfMap.App.HttpPort)
+}
 
 func GlobalConfToml() {
-	var GlobalConfPath = "/conf/global.toml"
+	GlobalConfPath := "/conf/global.toml"
 	//get api conf
-	driver.InitConfToml(GlobalConfPath, &types.GlobalConf)
-	fmt.Println(types.GlobalConf)
+	driver.InitConfToml(GlobalConfPath, &GlobalConf)
+	fmt.Println(GlobalConf)
 }
--- examples/test/types/types.go
+++ examples/test/types/types.go
@@ -1,7 +1,5 @@
 package types
 
-var GlobalConf GlobalConfMap
-
 type GlobalConfMap struct {
 	Global struct {
 		Name       string `toml:"name"`
@@ -9,4 +7,25 @@ type GlobalConfMap struct {
 		TimeFormat string `toml:"time_format"`
 		DateFormat string `toml:"date_format"`
 	} `toml:"global"`
+}
+
+type AppConfMap struct {
+	App struct {
+		HttpPort int    `toml:"http_port"`
+		Mode     string `toml:"mode"`
+		Limit    int    `toml:"limit"`
+	} `toml:"app"`
+	Domain struct {
+		AdminDomain string   `toml:"admin_domain"`
+		WebDomain   []string `toml:"web_domain"`
+		ImageUrl    string   `toml:"image_url"`
+	} `toml:"domain"`
+	Token struct {
+		TokenName   string `toml:"token_name"`
+		TokenExpire int    `toml:"token_expire"`
+	} `toml:"Token"`
+	Path struct {
+		LogPathRoot string `toml:"log_path_root"`
+		PathRoot    string `toml:"path_root"`
+	} `toml:"Path"`
 }
