88a012776ed14601c8b5c7075dc0d233e2641a2b
cabbage <chinesebigcabbage@gmail.com>
2023-06-01 15:03:26 +0800

    edit mysql gorm version

--- driver/mysql.go
+++ driver/mysql.go
@@ -2,50 +2,61 @@ package driver
 
 import (
 	"fmt"
+	"log"
+	"time"
+
 	"github.com/BurntSushi/toml"
 	_ "github.com/go-sql-driver/mysql"
-	"github.com/jinzhu/gorm"
-	"log"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
-var	MysqlConfMapList MysqlConnConfMap
+var MysqlConfMapList MysqlConnConfMap
 
 type MysqlConnConfMap struct {
 	Mysql map[string]*MysqlConnConf `toml:"mysql"`
 }
 
 type MysqlConnConf struct {
-	DriverName  string `toml:"driver_name"`
-	Host        string `toml:"host"`
-	Database    string `toml:"database"`
-	User        string `toml:"user"`
-	Password    string `toml:"password"`
-	Port        uint   `toml:"port"`
-	MaxIdleConn int    `toml:"max_idle_conn"` //用于设置闲置的连接数。
-	MaxOpenConn int    `toml:"max_open_conn"` //用于设置最大打开的连接数，默认值为0表示不限制
-	SqlLog      bool   `toml:"sql_log"`       //是否打印sql
+	DriverName      string `toml:"driver_name"`
+	Host            string `toml:"host"`
+	Database        string `toml:"database"`
+	User            string `toml:"user"`
+	Password        string `toml:"password"`
+	Port            uint   `toml:"port"`
+	MaxIdleConn     int    `toml:"max_idle_conn"`      //用于设置闲置的连接数。
+	MaxOpenConn     int    `toml:"max_open_conn"`      //用于设置最大打开的连接数，默认值为0表示不限制
+	ConnMaxIdleTime int    `toml:"conn_max_idle_time"` //链接空闲时长 超出杀死
+	ConnMaxLifetime int    `toml:"conn_max_life_time"` //链接复用时长
+	SqlLog          int    `toml:"sql_log"`            //日志级别 1 静默 2 错误信息 3 警告信息 4 所有信息
 }
 
 //open conn
 func (conf *MysqlConnConf) mysqlInit() *gorm.DB {
+	dsn := fmt.Sprintf(
+		"%s:%s@tcp(%s:%d)/%s?charset=utf8&parseTime=False&loc=Local",
+		conf.User,
+		conf.Password,
+		conf.Host,
+		conf.Port,
+		conf.Database)
 	db, err := gorm.Open(
-		conf.DriverName,
-		fmt.Sprintf(
-			"%s:%s@(%s:%d)/%s?charset=utf8&parseTime=False&loc=Local",
-			conf.User,
-			conf.Password,
-			conf.Host,
-			conf.Port,
-			conf.Database,
-		),
+		mysql.Open(dsn),
+		&gorm.Config{},
 	)
 	if err != nil {
 		log.Fatal("mysql connect failed:", err)
 	}
-	db.DB().SetMaxIdleConns(conf.MaxIdleConn) //max free(idle) conn
-	db.DB().SetMaxOpenConns(conf.MaxOpenConn) //max conn
-	db.SingularTable(true)
-	db.LogMode(conf.SqlLog) //sql run log whether output
+	sqlDB, err := db.DB()
+	if err != nil {
+		log.Fatal("mysql connect failed:", err)
+	}
+	sqlDB.SetMaxIdleConns(conf.MaxIdleConn) //max free(idle) conn
+	sqlDB.SetMaxOpenConns(conf.MaxOpenConn) //max conn
+	sqlDB.SetConnMaxIdleTime(time.Minute)   //max idle conn time
+	sqlDB.SetConnMaxLifetime(time.Hour)
+	db.Logger.LogMode(logger.LogLevel(conf.SqlLog)) //sql run log whether output
 	return db
 }
 
--- go.mod
+++ go.mod
@@ -4,7 +4,9 @@ go 1.16
 
 require (
 	github.com/BurntSushi/toml v0.3.1
-	github.com/go-sql-driver/mysql v1.6.0
+	github.com/go-sql-driver/mysql v1.7.1
 	github.com/gomodule/redigo v1.8.4
-	github.com/jinzhu/gorm v1.9.16
+	github.com/stretchr/testify v1.8.1 // indirect
+	gorm.io/driver/mysql v1.5.1
+	gorm.io/gorm v1.25.1
 )
